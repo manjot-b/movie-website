3dce83cb83273d525659577d1e0cde62b4cf27f1
Manjot Bal <mbal29@hotmail.com>
2018-06-20 23:50:13 -0600

    added a nav bar to admin movie page

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,7 +15,21 @@ exports.media_movie_get = (req, res) => {
 
 exports.media_movie_add_get = (req, res) => {
     res.render('admin_movie_add', {
-        title: 'Add Media',
+        title: 'Add Movie',
+        isAdmin: req.session.user.isAdmin
+    });
+}
+
+exports.media_movie_edit_get = (req, res) => {
+    res.render('admin_movie_edit', {
+        title: 'Edit Movie',
+        isAdmin: req.session.user.isAdmin
+    });
+}
+
+exports.media_movie_delete_get = (req, res) => {
+    res.render('admin_movie_delete', {
+        title: 'Delete Movie',
         isAdmin: req.session.user.isAdmin
     });
 }
@@ -26,7 +40,7 @@ exports.media_tvshow_get = (req, res) => {
 
 exports.media_tvshow_add_get = (req, res) => {
     res.render('admin_tvshow_add', {
-        title: 'Add Media',
+        title: 'Add TV Show',
         isAdmin: req.session.user.isAdmin
     });
 }
--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,10 @@ router.get('/media/movie', adminController.media_movie_get);
 
 router.get('/media/movie/add', adminController.media_movie_add_get);
 
+router.get('/media/movie/edit', adminController.media_movie_edit_get);
+
+router.get('/media/movie/delete', adminController.media_movie_delete_get);
+
 router.get('/media/tv_show', adminController.media_tvshow_get);
 
 router.get('/media/tv_show/add', adminController.media_tvshow_add_get);
