a69e7bd0f3945aae095a33872cfb5d52b75c815c
manjot-b <mbal29@hotmail.com>
2018-06-20 19:25:07 -0600

    fixed routes and changed admin page

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,31 +6,29 @@ exports.media_get = (req, res) => {
         res.status(401).send("You don't have the required privileges");
     }
 
-    res.render('admin_media', { 
-        title: 'Welcome ' + req.session.user.username + '!',
-        isAdmin: req.session.user.isAdmin
-    });
+    res.redirect('/admin/media/movie/add');
 };
 
-exports.media_add_get = (req, res) => {
-    res.render('admin_media_add', {
+exports.media_movie_get = (req, res) => {
+    res.redirect('/admin/media/movie/add')
+}
+
+exports.media_movie_add_get = (req, res) => {
+    res.render('admin_movie_add', {
         title: 'Add Media',
         isAdmin: req.session.user.isAdmin
-    })
+    });
 }
 
-exports.media_edit_get = (req, res) => {
-    res.render('admin_media_add', {
-        title: 'Edit Media',
-        isAdmin: req.session.user.isAdmin
-    })
+exports.media_tvshow_get = (req, res) => {
+    res.redirect('/admin/media/tv_show/add')
 }
 
-exports.media_delete_get = (req, res) => {
-    res.render('admin_media_add', {
-        title: 'Delete Media',
+exports.media_tvshow_add_get = (req, res) => {
+    res.render('admin_tvshow_add', {
+        title: 'Add Media',
         isAdmin: req.session.user.isAdmin
-    })
+    });
 }
 
 exports.users_get = (req, res) => {
--- routes/admin.js
+++ routes/admin.js
@@ -7,10 +7,12 @@ router.get('/media', adminController.media_get);
 
 router.get('/users', adminController.users_get);
 
-router.get('/media/add', adminController.media_add_get);
+router.get('/media/movie', adminController.media_movie_get);
 
-router.get('/media/edit', adminController.media_edit_get);
+router.get('/media/movie/add', adminController.media_movie_add_get);
 
-router.get('/media/delete', adminController.media_delete_get);
+router.get('/media/tv_show', adminController.media_tvshow_get);
+
+router.get('/media/tv_show/add', adminController.media_tvshow_add_get);
 
 module.exports = router;
